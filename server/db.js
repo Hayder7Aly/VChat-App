b3518d28a02b6dbe27009f90a46b4414c44a7616
Hayder7Aly <007harryjutt@gmail.com>
2022-03-07 19:57:41 +0500

    add functionaly that create database if not exist

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ const { sequelize } = require('./models');
 const connectToDB = async () => {
   try {
     await sequelize.authenticate();
+    await sequelize.sync({force : true})
     console.log('PSQL connected!');
   } catch (error) {
     console.log(error);
