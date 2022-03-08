7a5fec23974ab01e487962a135c2474f8a583e5e
Hayder7Aly <97211761+Hayder7Aly@users.noreply.github.com>
2022-03-08 20:04:29 +0500

    Update db.js
    implement more readability .

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { sequelize } = require('./models');
 const connectToDB = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({force : true})
+    await sequelize.sync()
     console.log('PSQL connected!');
   } catch (error) {
     console.log(error);
